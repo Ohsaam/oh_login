c485944faa6df9e9908d027a929a7942c2180bee
Ohsaam <ohjihwan123@naver.com>
2023-11-16 14:40:36 +0900

    API키값 변경

--- myBook/public/javascripts/firebase.js
+++ myBook/public/javascripts/firebase.js
@@ -1,32 +1,13 @@
-  //Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.1/firebase-app.js";
-  
-  const firebaseConfig = {
-    apiKey: "AIzaSyDSE3syxroAfuSFMPRlMzdGZ45uApN-W9k",
-    authDomain: "mybook-9b191.firebaseapp.com",
-    projectId: "mybook-9b191",
-    storageBucket: "mybook-9b191.appspot.com",
-    messagingSenderId: "442421738767",
-    appId: "1:442421738767:web:91a6ba77aafba53aa7d27a",
-    measurementId: "G-JCPGBGBBFJ"
-  };
-  //Initialize Firebase
-  export const app = initializeApp(firebaseConfig);
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.22.1/firebase-app.js';
 
-// import { initializeApp } from "firebase/app";
+const firebaseConfig = {
+  apiKey: "AIzaSyDivRgIo9WuIw38DIC1Sg9548stt3B0JZE",
+  authDomain: "ohfeeling-24714.firebaseapp.com",
+  projectId: "ohfeeling-24714",
+  storageBucket: "ohfeeling-24714.appspot.com",
+  messagingSeㅁnderId: "705156586029",
+  appId: "1:705156586029:web:94985f016e9a6e074bf6a4"
+};
 
-// // Your web app's Firebase configuration
-// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-// const firebaseConfig = {
-//   apiKey: "AIzaSyBWeR8UdEmK9aCGxKgBjaG0jK3euA4IWsw",
-//   authDomain: "mydatabase-e2522.firebaseapp.com",
-//   databaseURL: "https://mydatabase-e2522-default-rtdb.firebaseio.com",
-//   projectId: "mydatabase-e2522",
-//   storageBucket: "mydatabase-e2522.appspot.com",
-//   messagingSenderId: "573847235209",
-//   appId: "1:573847235209:web:76e5e4978f160c6a4e7458",
-//   measurementId: "G-TYG933L2N7"
-// };
-
-// // Initialize Firebase
-// export const app = initializeApp(firebaseConfig);
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig); 
